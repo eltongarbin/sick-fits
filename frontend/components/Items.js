67eea914ffecc0ef42e8e547629310ac8bce3917
Elton Garbin <eltongarbin@gmail.com>
2019-12-20 22:16:42 -0300

    React Meets GraphQL

---
+++ frontend/components/Items.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+import Item from './Item';
+
+const ALL_ITEMS_QUERY = gql`
+  query allItemsQuery {
+    items {
+      id
+      title
+      price
+      description
+      image
+      largeImage
+    }
+  }
+`;
+
+const Center = styled.div`
+  text-align: center;
+`;
+
+const ItemsList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 60px;
+  max-width: ${(props) => props.theme.maxWidth};
+  margin: 0 auto;
+`;
+
+const Items = () => {
+  const { data, error, loading } = useQuery(ALL_ITEMS_QUERY);
+
+  if (loading) {
+    return (
+      <Center>
+        <p>Loading...</p>
+      </Center>
+    );
+  }
+
+  if (error) {
+    return (
+      <Center>
+        <p>Error: {error.message}</p>
+      </Center>
+    );
+  }
+
+  return (
+    <Center>
+      <ItemsList>
+        {data.items.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ItemsList>
+    </Center>
+  );
+};
+
+export default Items;
