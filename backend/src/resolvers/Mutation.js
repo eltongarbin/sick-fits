f2323ff009e9a47308ba7599f00684443978fab2
Elton Garbin <eltongarbin@gmail.com>
2020-01-30 23:07:29 -0300

    User Signup and Permission Flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,8 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
-  async createItem(parent, args, ctx, info) {
+  async createItem(_parent, args, ctx, info) {
     // TODO: Check if they are logged in
 
     const item = await ctx.db.mutation.createItem(
@@ -13,7 +16,7 @@ const Mutations = {
 
     return item;
   },
-  updateItem(parent, args, ctx, info) {
+  updateItem(_parent, args, ctx, info) {
     // takes a copy of the updates
     const updates = { ...args };
     delete updates.id;
@@ -28,11 +31,27 @@ const Mutations = {
       info
     );
   },
-  async deleteItem(parent, args, ctx, info) {
+  async deleteItem(_parent, args, ctx, info) {
     const where = { id: args.id };
     const item = await ctx.db.query.item({ where }, `{ id, title }`);
 
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(_parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    const password = await bcrypt.hash(args.password, 10);
+    const user = await ctx.db.mutation.createUser(
+      { data: { ...args, password, permissions: { set: ['USER'] } } },
+      info
+    );
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+
+    return user;
   }
 };
 
