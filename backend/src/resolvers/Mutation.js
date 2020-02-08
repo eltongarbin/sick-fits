5f26ed4d02d0825dc9fc7b9c6ef4e499101d77b8
Elton Garbin <eltongarbin@gmail.com>
2020-02-08 17:33:36 -0300

    Data Relationships

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -7,11 +7,13 @@ const { transport, makeANiceEmail } = require('../mail');
 
 const Mutations = {
   async createItem(_parent, args, ctx, info) {
-    // TODO: Check if they are logged in
+    if (!ctx.request.userId)
+      throw new Error('You must be logged in to do that!');
 
     const item = await ctx.db.mutation.createItem(
       {
         data: {
+          user: { connect: { id: ctx.request.userId } },
           ...args
         }
       },
