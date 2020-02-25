7ee8b9cac1c042f2f6b6fb59424f871feccf3a70
Elton Garbin <eltongarbin@gmail.com>
2020-02-25 01:30:29 -0300

    Animating Cart Count Component

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -5,6 +5,7 @@ import { useMutation } from '@apollo/react-hooks';
 import NavStyles from './styles/NavStyles';
 import useCurrentUserQuery from './hooks/useCurrentUserQuery';
 import Signout from './Signout';
+import CartCount from './CartCount';
 import { TOGGLE_CART_MUTATION } from './Cart';
 
 const Nav = () => {
@@ -30,7 +31,15 @@ const Nav = () => {
             <a>Account</a>
           </Link>
           <Signout />
-          <button onClick={toggleCart}>My Cart</button>
+          <button onClick={toggleCart}>
+            My Cart{' '}
+            <CartCount
+              count={data.me.cart.reduce(
+                (tally, cartItem) => tally + cartItem.quantity,
+                0
+              )}
+            />
+          </button>
         </>
       )}
       {!data?.me && (
