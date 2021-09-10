0a1afe00de04900a302871264f46c0a71dd08ba3
Elton Garbin <eltongarbin@gmail.com>
2021-09-09 21:55:07 -0300

    chore: reset project

--- frontend/components/ErrorMessage.js
+++ frontend/components/ErrorMessage.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import React from 'react';
+
+import PropTypes from 'prop-types';
 
 const ErrorStyles = styled.div`
   padding: 2rem;
@@ -19,11 +20,7 @@ const ErrorStyles = styled.div`
 
 const DisplayError = ({ error }) => {
   if (!error || !error.message) return null;
-  if (
-    error.networkError &&
-    error.networkError.result &&
-    error.networkError.result.errors.length
-  ) {
+  if (error.networkError && error.networkError.result && error.networkError.result.errors.length) {
     return error.networkError.result.errors.map((error, i) => (
       <ErrorStyles key={i}>
         <p data-test="graphql-error">
@@ -44,11 +41,11 @@ const DisplayError = ({ error }) => {
 };
 
 DisplayError.defaultProps = {
-  error: {}
+  error: {},
 };
 
 DisplayError.propTypes = {
-  error: PropTypes.object
+  error: PropTypes.object,
 };
 
 export default DisplayError;
