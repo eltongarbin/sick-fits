0b08abae2dcc4269523311c6c1609d0660437f71
Elton Garbin <eltongarbin@gmail.com>
2022-04-21 21:41:01 -0300

    chore: create User data type

---
+++ backend/schemas/User.ts
@@ -0,0 +1,14 @@
+import { password, text } from "@keystone-next/fields";
+import { list } from "@keystone-next/keystone/schema";
+
+// Named export
+export const User = list({
+  // access
+  // ui
+  fields: {
+    name: text({ isRequired: true }),
+    email: text({ isRequired: true, isUnique: true }),
+    password: password(),
+    // TODO: add roles, cart and orders
+  },
+});
