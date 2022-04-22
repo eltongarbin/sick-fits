a5820d2107e841e6888d113d31d9d9c29d54a43b
Elton Garbin <eltongarbin@gmail.com>
2022-04-22 11:19:22 -0300

    fix: adjust nav styles

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -24,7 +24,7 @@ const HeaderStyles = styled.header`
     display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
-    align-items: center;
+    align-items: stretch;
   }
 
   .sub-bar {
@@ -37,16 +37,15 @@ const HeaderStyles = styled.header`
 export default function Header() {
   return (
     <HeaderStyles>
-      <p>I'm the header</p>
       <div className="bar">
         <Logo>
           <Link href="/">Sick fits</Link>
         </Logo>
+        <Nav />
       </div>
       <div className="sub-bar">
-        <p>Sick fits</p>
+        <p>Search</p>
       </div>
-      <Nav />
     </HeaderStyles>
   );
 }
