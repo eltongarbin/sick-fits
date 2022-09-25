59c8232a03e8a893ab6bcce46448217de0c5cfb8
Elton Garbin <eltongarbin@gmail.com>
2022-09-24 23:59:17 -0300

    feat(cart): remove from cart button

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -7,6 +7,7 @@ import { useUser } from "./User";
 import formatMoney from "./../lib/formatMoney";
 import calcTotalPrice from "../lib/calcTotalPrice";
 import { useCart } from "../lib/cartState";
+import RemoveFromCart from "./RemoveFromCart";
 
 const CartItemStyles = styled.li`
   padding: 1rem 0;
@@ -45,6 +46,7 @@ function CartItem({ cartItem }) {
           </em>
         </p>
       </div>
+      <RemoveFromCart id={cartItem.id} />
     </CartItemStyles>
   );
 }
