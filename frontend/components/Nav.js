44c7454d9378449a08a5a9af4b87e890f0a7ec58
Elton Garbin <eltongarbin@gmail.com>
2022-09-24 23:51:56 -0300

    feat(cart): animating cart value

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,6 +4,7 @@ import NavStyles from "./styles/NavStyles";
 import { useUser } from "./User";
 import SignOut from "./SignOut";
 import { useCart } from "../lib/cartState";
+import CartCount from "./CartCount";
 
 export default function Nav() {
   const user = useUser();
@@ -20,6 +21,12 @@ export default function Nav() {
           <SignOut />
           <button type="button" onClick={openCart}>
             My Cart
+            <CartCount
+              count={user.cart.reduce(
+                (tally, cartItem) => tally + cartItem.quantity,
+                0
+              )}
+            />
           </button>
         </>
       )}
