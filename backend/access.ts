399b498e24a1a9ebfd66d961043b77d06baadc76
Elton Garbin <eltongarbin@gmail.com>
2022-11-03 18:09:40 -0300

    chore: setup access control

---
+++ backend/access.ts
@@ -0,0 +1,30 @@
+import { permissionsList } from "./schemas/fields";
+import { ListAccessArgs } from "./types";
+
+export const isSignedIn = ({ session }: ListAccessArgs) => !!session;
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    ({ session }: ListAccessArgs) => !!session?.data.role?.[permission],
+  ])
+);
+
+export const permissions = { ...generatedPermissions };
+
+export const rules = {
+  canManageProducts({ session }: ListAccessArgs) {
+    if (permissions.canManageProducts({ session })) {
+      return true;
+    }
+
+    return { user: { id: session.itemId } };
+  },
+  canReadProducts({ session }: ListAccessArgs) {
+    if (permissions.canManageProducts({ session })) {
+      return true;
+    }
+
+    return { status: "AVAILABLE" };
+  },
+};
